6360cd140df376ce49d9489b0ae8003afa540eb1
Bitopan Das <bitopan21mbb@gmail.com>
2025-06-28 13:20:37 +0530

    locality

--- views/components/prebook_templ.go
+++ views/components/prebook_templ.go
@@ -422,14 +422,14 @@ func LocalityOptions(localities []string) templ.Component {
 		}
 		ctx = templ.ClearChildren(ctx)
 		for _, l := range localities {
-			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 37, "<option value=\"{{l}}\">")
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 37, "<option value=\"{l}\">")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
 			var templ_7745c5c3_Var19 string
 			templ_7745c5c3_Var19, templ_7745c5c3_Err = templ.JoinStringErrs(l)
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `views/components/prebook.templ`, Line: 205, Col: 32}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `views/components/prebook.templ`, Line: 205, Col: 30}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var19))
 			if templ_7745c5c3_Err != nil {
