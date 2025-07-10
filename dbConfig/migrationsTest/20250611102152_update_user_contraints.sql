04d4abdd54504e6010dea73f98c885e5681b9fcf
Bitopan Das <bitopan21mbb@gmail.com>
2025-07-10 19:55:33 +0530

    removed dbConfig

--- dbConfig/migrationsTest/20250611102152_update_user_contraints.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-ALTER TABLE users DROP CONSTRAINT IF EXISTS user_email_key;
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-ALTER TABLE users ADD CONSTRAINT user_email_key UNIQUE (email);
--- +goose StatementEnd
--- dbConfig/queries.sql
+++
@@ -1,2 +0,0 @@
--- name: InsertUserTest :one 
-INSERT INTO prebook_data(uid, firstname,lastname,email,phone,city,locality) VALUES($1, $2, $3, $4, $5, $6,$7) RETURNING *;
